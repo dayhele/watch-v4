f8a1942763f2cc0ad29514f4f555b22bb1297d2b
dayhele <dayhelepereira@gmail.com>
2023-01-17 09:46:42 -0300

    adding kaltura to the project

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-// Bootstrap CSS
-import "bootstrap/dist/css/bootstrap.min.css";
-// Bootstrap Bundle JS
-import "bootstrap/dist/js/bootstrap.bundle.min";
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 

c4f075cb8bd2b743e60b1fd39728661331f5e3b3
dayhele <dayhelepereira@gmail.com>
2023-02-10 10:36:15 -0300

    creating footer component and moving manifest.json to public folder

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import style from "./styles";
+
+const Footer = () => {
+  return (
+  <>
+  </>
+  );
+};
+
+export default Footer;
---
+++ src/components/Footer/styles.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const FooterWrapper = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+`;
+
+export const FooterContentWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+`;
